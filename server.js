c159303239d208339584decd05126005e88357c4
mahmoudnader150 <mahnader222@gmail.com>
2024-01-26 20:04:54 +0200

    fix some errors and add some dependencies

---
+++ server.js
@@ -0,0 +1,35 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable import/newline-after-import */
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const app = require('./app');
+
+dotenv.config({ path: './config.env' });
+console.log(process.env);
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true, 
+   // useUnifiedTopology: true, // Adding the unified topology option
+  })
+  .then((con) => {
+ //   console.log(con.connections);
+    console.log('DB connection successful');
+  })
+  .catch((err) => {
+    console.error('Error connecting to the database:', err);
+  });
+
+
+
+  
+
+const port = 8000;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+ 
